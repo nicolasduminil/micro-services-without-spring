530578d877f70634499da57882869bc59ef5bd35
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2021-03-10 19:35:12 +0100

    Test PAT

--- customers/customization/run.sh
+++ customers/customization/run.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 echo "************************************************************"
 echo "Waiting for the Oracle database server to start on port $ORACLE_PORT"
 echo "************************************************************"
